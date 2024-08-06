3600d99e2d82c6d6c389d9ce150254be84b9c50e
Javier Rojas <javierito.rojas.ramirez@gmail.com>
2024-08-06 15:33:00 +0100

    Enemy hp n' HitVFX: Added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 10;
 
     ScoreBoard _scoreBoard;
 
@@ -17,16 +19,22 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (hitPoints < 1) { KillEnemy(); }
     }
 
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        
+        hitPoints--;
         _scoreBoard.IncreaseScore(scorePerHit);
     }
 
     void KillEnemy()
     {
+        _scoreBoard.IncreaseScore(scorePerHit * 2);
+        
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
